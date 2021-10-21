c69fc76a53ec602540337b651c1ef984fb1723a6
ganesh1076 <pawarganesh1076@gmail.com>
2021-10-22 03:01:03 +0530

    modification

--- git_push.py
+++ git_push.py
@@ -1,5 +1,5 @@
 import os
 os.system("git add .")
 os.system("git commit -mmodification")
-os.system(" git push -f origin HEAD:master")
-#os.system(" git push -f origin HEAD:main")
+#os.system(" git push -f origin HEAD:master")
+os.system(" git push -f origin HEAD:main")
